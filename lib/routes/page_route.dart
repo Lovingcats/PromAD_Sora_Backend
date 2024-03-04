b36537dc466d16656d3d8275e1bef05c6c0b9556
Lovingcats <whehdrms6810@naver.com>
2024-03-04 22:38:00 +0900

    [ADD] 아래에 위로 올라우는 라우트 생성

---
+++ lib/routes/page_route.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomPageRoute<T> extends PageRouteBuilder<T> {
+  final Widget child;
+  final Duration duration;
+
+  CustomPageRoute({
+    required this.child,
+    this.duration = const Duration(milliseconds: 200),
+  }) : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              child,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) {
+            var begin = const Offset(0.0, 1.0);
+            var end = Offset.zero;
+            var curve = Curves.easeOutCubic;
+
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: child,
+            );
+          },
+          transitionDuration: duration,
+        );
+}
