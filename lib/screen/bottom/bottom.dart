a7f4fce737342d5a96200c357483d76750d55d13
Lovingcats <whehdrms6810@naver.com>
2024-03-04 10:45:33 +0900

    [CREATE] bottom.dart

---
+++ lib/screen/bottom/bottom.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatefulWidget {
+  const Bottom({super.key});
+
+  @override
+  State<Bottom> createState() => _BottomState();
+}
+
+class _BottomState extends State<Bottom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hello"),
+      ),
+    );
+  }
+}
