4ba4c9a7b155fac9eb162a5ab9845c3e359a176a
Lovingcats <whehdrms6810@naver.com>
2024-03-06 08:53:56 +0900

    [ADD] 버튼 물결 이벤트 제거를 위한 themedata생성

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:promad_sora/common/common.dart';
 import 'package:promad_sora/provider/user_provider.dart';
 import 'package:promad_sora/screen/introslider/introslider.dart';
-import 'package:promad_sora/widget/introslider.dart';
 import 'package:provider/provider.dart';
 import 'package:intro_slider/intro_slider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -30,9 +29,13 @@ class MyApp extends StatelessWidget {
       child: ScreenUtilInit(
         //screenutil 라이브러리 (뒤에 .h, .w00, .r, .sp등등 크기를 반응형으로 만들어줌)
         designSize: const Size(360, 800), // 어떤 사이즈를 기준으로 만들것인가
-        builder: (BuildContext context, Widget? child) => const MaterialApp(
+        builder: (BuildContext context, Widget? child) => MaterialApp(
             title: 'PromAD_sora',
             debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
             home: IntroduceSlider()),
       ),
     );
