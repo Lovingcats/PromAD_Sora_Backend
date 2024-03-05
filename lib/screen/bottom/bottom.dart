3f5719a17bd5808975c0bef71603f73d0f0b2379
Lovingcats <whehdrms6810@naver.com>
2024-03-06 08:54:52 +0900

    [UPDATE] bottombar 애니메이션 수정중

--- lib/screen/bottom/bottom.dart
+++ lib/screen/bottom/bottom.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:promad_sora/common/common.dart';
 import 'package:promad_sora/screen/bottom/home.dart';
 import 'package:promad_sora/screen/bottom/menu.dart';
 import 'package:promad_sora/screen/bottom/profile.dart';
-import 'package:provider/provider.dart';
 import 'package:stylish_bottom_bar/model/bar_items.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
@@ -39,161 +38,12 @@ class _HomePageState extends State<Bottom> {
     return Stack(
       children: [
         DefaultTabController(
-            length: 4,
+            length: 3,
             child: Scaffold(
+              backgroundColor: backgroundColor,
+              appBar: null,
               extendBody: true,
-              bottomNavigationBar: Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    top: BorderSide(
-                        width: 0.2.w,
-                        color: const Color.fromARGB(255, 81, 81, 81)
-                            .withOpacity(0.2)), // 흰색 테두리 추가
-                  ),
-                ),
-                child: StylishBottomBar(
-                  backgroundColor: backgroundColor,
-                  items: [
-                    BottomBarItem(
-                        selectedColor: Colors.black,
-                        backgroundColor: Colors.white,
-                        icon: Text(
-                          String.fromCharCode(
-                              CupertinoIcons.house_fill.codePoint),
-                          style: TextStyle(
-                            inherit: false,
-                            color: const Color.fromARGB(255, 92, 91, 91),
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w100,
-                            fontFamily: CupertinoIcons
-                                .exclamationmark_circle.fontFamily,
-                            package: CupertinoIcons
-                                .exclamationmark_circle.fontPackage,
-                          ),
-                        ),
-                        selectedIcon: Text(
-                          String.fromCharCode(
-                              CupertinoIcons.house_fill.codePoint),
-                          style: TextStyle(
-                            inherit: false,
-                            color: Colors.white,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w100,
-                            fontFamily: CupertinoIcons
-                                .exclamationmark_circle.fontFamily,
-                            package: CupertinoIcons
-                                .exclamationmark_circle.fontPackage,
-                          ),
-                        ),
-                        title: Text(
-                          'Home',
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontFamily: 'Nexa_Regular',
-                            color: selected == 0
-                                ? Colors.white
-                                : const Color.fromARGB(255, 92, 91, 91),
-                          ),
-                        )),
-                    BottomBarItem(
-                        selectedColor: Colors.black,
-                        backgroundColor: Colors.white,
-                        icon: Text(
-                          String.fromCharCode(
-                              CupertinoIcons.square_list_fill.codePoint),
-                          style: TextStyle(
-                            inherit: false,
-                            color: const Color.fromARGB(255, 92, 91, 91),
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w100,
-                            fontFamily: CupertinoIcons
-                                .exclamationmark_circle.fontFamily,
-                            package: CupertinoIcons
-                                .exclamationmark_circle.fontPackage,
-                          ),
-                        ),
-                        selectedIcon: Text(
-                          String.fromCharCode(
-                              CupertinoIcons.square_list_fill.codePoint),
-                          style: TextStyle(
-                            inherit: false,
-                            color: Colors.white,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w100,
-                            fontFamily: CupertinoIcons
-                                .exclamationmark_circle.fontFamily,
-                            package: CupertinoIcons
-                                .exclamationmark_circle.fontPackage,
-                          ),
-                        ),
-                        title: Text(
-                          'Tools',
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontFamily: 'Nexa_Regular',
-                            color: selected == 1
-                                ? Colors.white
-                                : const Color.fromARGB(255, 92, 91, 91),
-                          ),
-                        )),
-                    BottomBarItem(
-                        selectedColor: Colors.black,
-                        backgroundColor: Colors.white,
-                        icon: Text(
-                          String.fromCharCode(
-                              CupertinoIcons.profile_circled.codePoint),
-                          style: TextStyle(
-                            inherit: false,
-                            color: const Color.fromARGB(255, 92, 91, 91),
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w100,
-                            fontFamily: CupertinoIcons
-                                .exclamationmark_circle.fontFamily,
-                            package: CupertinoIcons
-                                .exclamationmark_circle.fontPackage,
-                          ),
-                        ),
-                        selectedIcon: Text(
-                          String.fromCharCode(
-                              CupertinoIcons.profile_circled.codePoint),
-                          style: TextStyle(
-                            inherit: false,
-                            color: Colors.white,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w100,
-                            fontFamily: CupertinoIcons
-                                .exclamationmark_circle.fontFamily,
-                            package: CupertinoIcons
-                                .exclamationmark_circle.fontPackage,
-                          ),
-                        ),
-                        title: Text(
-                          'Profile',
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontFamily: 'Nexa_Regular',
-                            color: selected == 2
-                                ? Colors.white
-                                : const Color.fromARGB(255, 92, 91, 91),
-                          ),
-                        )),
-                  ],
-                  option: AnimatedBarOptions(
-                    iconSize: 23.h,
-                    barAnimation: BarAnimation.fade,
-                    iconStyle: IconStyle.Default,
-                    opacity: 0.3,
-                  ),
-                  currentIndex: selected ?? 0,
-                  hasNotch: true,
-                  onTap: (index) {
-                    controller.jumpToPage(index);
-                    setState(() {
-                      selected = index;
-                    });
-                  },
-                ),
-              ),
+              bottomNavigationBar: bottombar(),
               body: SafeArea(
                 child: Stack(
                   children: [
@@ -219,13 +69,173 @@ class _HomePageState extends State<Bottom> {
                           });
                         }
                       }),
-                      children: const [Home(), Menu(), Profile()],
+                      children: [
+                        AnimatedOpacity(
+                            duration: const Duration(milliseconds: 10000),
+                            opacity: selected == 0 ? 1 : 0,
+                            child: Home()),
+                        AnimatedOpacity(
+                            duration: const Duration(milliseconds: 10000),
+                            opacity: selected == 1 ? 1 : 0,
+                            child: Menu()),
+                        AnimatedOpacity(
+                            duration: const Duration(milliseconds: 10000),
+                            opacity: selected == 2 ? 1 : 0,
+                            child: Profile()),
+                      ],
                     ),
                   ],
                 ),
               ),
             )),
       ],
+    );
+  }
+
+  Theme bottombar() {
+    return Theme(
+      data: ThemeData(),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+                width: 0.2.w,
+                color: const Color.fromARGB(255, 79, 79, 79)), // 흰색 테두리 추가
+          ),
+        ),
+        child: StylishBottomBar(
+          backgroundColor: backgroundColor,
+          items: [
+            BottomBarItem(
+                selectedColor: Colors.black,
+                backgroundColor: Colors.white,
+                icon: Text(
+                  String.fromCharCode(CupertinoIcons.house_fill.codePoint),
+                  style: TextStyle(
+                    inherit: false,
+                    color: const Color.fromARGB(255, 92, 91, 91),
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w100,
+                    fontFamily:
+                        CupertinoIcons.exclamationmark_circle.fontFamily,
+                    package: CupertinoIcons.exclamationmark_circle.fontPackage,
+                  ),
+                ),
+                selectedIcon: Text(
+                  String.fromCharCode(CupertinoIcons.house_fill.codePoint),
+                  style: TextStyle(
+                    inherit: false,
+                    color: Colors.white,
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w100,
+                    fontFamily:
+                        CupertinoIcons.exclamationmark_circle.fontFamily,
+                    package: CupertinoIcons.exclamationmark_circle.fontPackage,
+                  ),
+                ),
+                title: Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontFamily: 'Nexa_Regular',
+                    color: selected == 0
+                        ? Colors.white
+                        : const Color.fromARGB(255, 92, 91, 91),
+                  ),
+                )),
+            BottomBarItem(
+                selectedColor: Colors.black,
+                backgroundColor: Colors.white,
+                icon: Text(
+                  String.fromCharCode(
+                      CupertinoIcons.square_list_fill.codePoint),
+                  style: TextStyle(
+                    inherit: false,
+                    color: const Color.fromARGB(255, 92, 91, 91),
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w100,
+                    fontFamily:
+                        CupertinoIcons.exclamationmark_circle.fontFamily,
+                    package: CupertinoIcons.exclamationmark_circle.fontPackage,
+                  ),
+                ),
+                selectedIcon: Text(
+                  String.fromCharCode(
+                      CupertinoIcons.square_list_fill.codePoint),
+                  style: TextStyle(
+                    inherit: false,
+                    color: Colors.white,
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w100,
+                    fontFamily:
+                        CupertinoIcons.exclamationmark_circle.fontFamily,
+                    package: CupertinoIcons.exclamationmark_circle.fontPackage,
+                  ),
+                ),
+                title: Text(
+                  'Tools',
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontFamily: 'Nexa_Regular',
+                    color: selected == 1
+                        ? Colors.white
+                        : const Color.fromARGB(255, 92, 91, 91),
+                  ),
+                )),
+            BottomBarItem(
+                selectedColor: Colors.black,
+                backgroundColor: Colors.white,
+                icon: Text(
+                  String.fromCharCode(CupertinoIcons.profile_circled.codePoint),
+                  style: TextStyle(
+                    inherit: false,
+                    color: const Color.fromARGB(255, 92, 91, 91),
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w100,
+                    fontFamily:
+                        CupertinoIcons.exclamationmark_circle.fontFamily,
+                    package: CupertinoIcons.exclamationmark_circle.fontPackage,
+                  ),
+                ),
+                selectedIcon: Text(
+                  String.fromCharCode(CupertinoIcons.profile_circled.codePoint),
+                  style: TextStyle(
+                    inherit: false,
+                    color: Colors.white,
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w100,
+                    fontFamily:
+                        CupertinoIcons.exclamationmark_circle.fontFamily,
+                    package: CupertinoIcons.exclamationmark_circle.fontPackage,
+                  ),
+                ),
+                title: Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    fontFamily: 'Nexa_Regular',
+                    color: selected == 2
+                        ? Colors.white
+                        : const Color.fromARGB(255, 92, 91, 91),
+                  ),
+                )),
+          ],
+          option: AnimatedBarOptions(
+            iconSize: 23.h,
+            barAnimation: BarAnimation.fade,
+            iconStyle: IconStyle.Default,
+            opacity: 0.3,
+          ),
+          currentIndex: selected ?? 0,
+          hasNotch: true,
+          onTap: (index) {
+            controller.jumpToPage(index);
+            setState(() {
+              selected = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
