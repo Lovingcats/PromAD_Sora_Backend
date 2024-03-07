42f96eeaece62abfa54d82d31d55d282901cd793
Lovingcats <whehdrms6810@naver.com>
2024-03-07 23:00:45 +0900

    [UPDATE] Theme 위젯 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,9 @@ class MyApp extends StatelessWidget {
       child: ScreenUtilInit(
         //screenutil 라이브러리 (뒤에 .h, .w00, .r, .sp등등 크기를 반응형으로 만들어줌)
         designSize: const Size(360, 800), // 어떤 사이즈를 기준으로 만들것인가
-        builder: (BuildContext context, Widget? child) => MaterialApp(
+        builder: (BuildContext context, Widget? child) => const MaterialApp(
             title: 'PromAD_sora',
             debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-            ),
             home: IntroduceSlider()),
       ),
     );
