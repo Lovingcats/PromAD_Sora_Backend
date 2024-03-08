a144b12b2fb24836a36f383be4c05b1dfc344f88
Lovingcats <whehdrms6810@naver.com>
2024-03-08 20:58:57 +0900

    [ADD] subColors

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const mainColor = Color(0xff00FFFF);
 const backgroundColor = Color(0xff1f1e23);
+const backSubColor1 = Color.fromARGB(255, 92, 91, 91);
+const backSubColor2 = Color.fromARGB(255, 55, 55, 55);
