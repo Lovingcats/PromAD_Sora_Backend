c7d60a32669d83acf3916f4d880239cc2c2e75a3
Lovingcats <whehdrms6810@naver.com>
2024-03-18 20:13:15 +0900

    [CREATE] style_provider.dart

---
+++ lib/provider/style_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:promad_sora/model/videostyle_model.dart';
+
+class StyleProvider extends ChangeNotifier {
+  final List<SelectVideoStyleModel> _selectVideoStyleModel =
+      <SelectVideoStyleModel>[];
+
+  List<SelectVideoStyleModel> get selectVideoStyleModel =>
+      _selectVideoStyleModel;
+
+  void addlist(String styleName, String imageUrl, bool isSelected) {
+    selectVideoStyleModel
+        .add(SelectVideoStyleModel(styleName, imageUrl, isSelected));
+    notifyListeners();
+  }
+
+  void clearList() {
+    selectVideoStyleModel.clear();
+    notifyListeners();
+  }
+}
