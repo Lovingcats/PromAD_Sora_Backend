bd424aad5bba6dd6978952b2ebb43249d40a7254
Lovingcats <whehdrms6810@naver.com>
2024-03-20 22:16:39 +0900

    [ADD] styleProvider

--- lib/widgets/selectstyle_slider.dart
+++ lib/widgets/selectstyle_slider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:promad_sora/common/common.dart';
 import 'package:promad_sora/model/videostyle_model.dart';
+import 'package:promad_sora/provider/style_provider.dart';
+import 'package:provider/provider.dart';
 
 class SelectVideoStyleSlider extends StatefulWidget {
   const SelectVideoStyleSlider({super.key});
@@ -11,41 +13,45 @@ class SelectVideoStyleSlider extends StatefulWidget {
 }
 
 class _SelectVideoStyleSliderState extends State<SelectVideoStyleSlider> {
-  List<SelectVideoStyleModel> selectVideoStyleModel = <SelectVideoStyleModel>[];
   @override
   void initState() {
-    List<String> styleName = [
-      "Cat1",
-      "Cat2",
-      "Cat3",
-      "Cat4",
-      "Cat5",
-      "Cat6",
-      "Cat7",
-      "Cat8"
-    ];
+    print("실행");
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      List<String> styleName = [
+        "Cat1",
+        "Cat2",
+        "Cat3",
+        "Cat4",
+        "Cat5",
+        "Cat6",
+        "Cat7",
+        "Cat8"
+      ];
+      var styleProvider = Provider.of<StyleProvider>(context, listen: false);
+      for (int i = 0; i < 8; i++) {
+        styleProvider.addList(
+            styleName[i], "assets/img/cat${i + 1}.jpeg", i == 0 ? true : false);
+      }
+    });
 
-    for (int i = 0; i < 8; i++) {
-      selectVideoStyleModel.add(SelectVideoStyleModel(
-          styleName[i], "assets/img/cat${i + 1}.jpeg", i == 0 ? true : false));
-    }
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    var styleProvider = Provider.of<StyleProvider>(context);
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: makeStyleWidget(),
+        children: makeStyleWidget(styleProvider),
       ),
     );
   }
 
-  List<Widget> makeStyleWidget() {
+  List<Widget> makeStyleWidget(StyleProvider styleProvider) {
+    var selectVideoStyleModel = styleProvider.selectVideoStyleModel;
     List<Widget> results = [];
-
     for (var i = 0; i < selectVideoStyleModel.length; i++) {
       bool isSelected = selectVideoStyleModel[i].isSelected;
 
