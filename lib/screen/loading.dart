ed417bbaa5faeda63a3b8f0aef911b01c0dcf765
Lovingcats <whehdrms6810@naver.com>
2024-04-24 21:46:49 +0900

    [DELETE] 불필요한 import 삭제

--- lib/screen/loading.dart
+++ lib/screen/loading.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:promad_sora/common/common.dart';
