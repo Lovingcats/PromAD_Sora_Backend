2df1b3a8384a82c6fdc5cdedd8e99fe314eb4814
Lovingcats <whehdrms6810@naver.com>
2024-06-07 12:33:53 +0900

    [UPDATE] FlutterActivity -> FlutterFargmentActivity

--- android/app/src/main/kotlin/com/example/promad_sora/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/promad_sora/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.promad_sora
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
